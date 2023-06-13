8106421a259a74d46f1799d2192c5e77fdef6a57
LeoLuca97 <37438630+CubeDevelope@users.noreply.github.com>
2023-06-14 00:13:59 +0200

    Add dependences and create base pages of project

---
+++ personal_finance_manager/lib/views/new_transaction.page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class NewTransactionPage extends StatefulWidget {
+  const NewTransactionPage({super.key});
+
+  @override
+  State<NewTransactionPage> createState() => _NewTransactionPageState();
+}
+
+class _NewTransactionPageState extends State<NewTransactionPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
